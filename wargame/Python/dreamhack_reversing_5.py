f14b798a13a5d28234ee732c22863f512c1682be
Persian <kr.cs.project.59@gmail.com>
2021-03-01 17:41:51 +0900

    Mon 01 Mar 2021 05:41:51 PM KST

--- wargame/Python/dreamhack_reversing_5.py
+++ wargame/Python/dreamhack_reversing_5.py
@@ -3,6 +3,6 @@
 import hextoList as htL
 
 refBF = "ADD8CBCB9D97CBC492A1D2D7D2D6A8A5DCC7ADA3A1984C"
-refAF = htL.hexStr(refBF)
+refAF = ['0x' + val for val in htL.hexStr(refBF)]
 
-print(refAF)
+print(hex(refAF[22])-hex(refAF[21]))
