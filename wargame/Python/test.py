90f7c9cde558d3b8314d15b1ab7610735a4ad332
Jun-Project-LAB <kr.cs.project.59@gmail.com>
2021-10-11 17:36:05 +0900

    Remove Trash

--- wargame/Python/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-a = 0xf0
-
-b = a << 1
-
-c = (b & 0x0ff) | 0x1
-
-print(hex(b), hex(c))
--- wargame/Python/ttest.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-from pwn import *
-
-conn = pwn.remote("host1.dreamhack.games", 23419)
